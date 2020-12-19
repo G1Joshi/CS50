1d208fcb9c3f23ff43fc4238c6113d233e27c967
Jeevan Chandra Joshi <52780516+G1Joshi@users.noreply.github.com>
2020-12-19 17:48:29 +0530

    Create mario.py

---
+++ Python/Problem Set 6/Mario/mario(more).py
@@ -0,0 +1,17 @@
+import cs50
+
+h = cs50.get_int("Height: ")
+
+if (h < 1 or h > 8):
+    while (h < 1 or h > 8):
+        h = cs50.get_int("Height: ")
+
+for i in range(h):
+    for j in range(h-1, i, -1):
+        print(" ", end="")
+    for j in range(i, -1, -1):
+        print("#", end="")
+    print("  ", end="")
+    for j in range(0, i+1):
+        print("#", end="")
+    print("")
